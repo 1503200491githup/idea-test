c1abebf5af8a5196b14063c6a1be7845d6e5334b
wen <1503200491@qq.com>
2019-12-28 23:31:53 +0800

    branch new

--- src/main/java/cn/git/TestGit.java
+++ src/main/java/cn/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("嘿嘿嘿git");
         System.out.println("嗨佬 我克隆完成了  发送回去给你！");
+        System.out.println("666");
     }
 }
