ce7f0e94d1cb696c0b15ee3c3a8de3905fa6d2ec
wen <1503200491@qq.com>
2019-12-28 23:20:19 +0800

    new idea test

--- src/main/java/cn/git/TestGit.java
+++ src/main/java/cn/git/TestGit.java
@@ -3,5 +3,6 @@ package cn.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("嘿嘿嘿git");
+        System.out.println("嗨佬 我克隆完成了  发送回去给你！");
     }
 }
